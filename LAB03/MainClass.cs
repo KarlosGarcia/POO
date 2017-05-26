c276a60a3e584a8efbfb93a152214766946bb478
1017824 <1017824@50HW002787>
2017-05-26 11:20:36 -0500

    LAB03

---
+++ LAB03/MainClass.cs
@@ -0,0 +1,50 @@
+﻿using Lab03;
+using System;
+using System.Collections.Generic;
+
+namespace LAB03
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var debs = new List<IPay>();
+
+            debs.Add(new Invoice
+            {
+                Description = "Coke",
+                Id = "1010",
+                Price = 1.32M,
+                Quantity = 48,
+            });
+
+            debs.Add(new Invoice
+            {
+                Description = "Milk",
+                Id = "2020",
+                Price = 1.22M,
+                Quantity = 12,
+            });
+
+            debs.Add(new SalariedEmployee {
+                FirstName = "Esperanza",
+                Id = "1017825",
+                LastName = "Gomez",
+                Salary = 300000, 
+            });
+
+            decimal total = 0;
+            foreach (var item in debs)
+            {
+                Console.WriteLine(item);
+                total += item.GetAmountToPay();
+            }
+
+            Console.WriteLine("                                 ==================");
+            Console.WriteLine("TOTAL..........................: {0,18:C2}", total);
+            Console.WriteLine("Press any key to finish...");
+            Console.ReadKey();
+            Console.WriteLine("Hasta la vista baby");
+        }
+    }
+}
---
+++ LAB03/SalariedEmployee.cs
@@ -0,0 +1,22 @@
+﻿using Lab03;
+using System;
+
+
+namespace LAB03
+{
+    public class SalariedEmployee : Employee
+    {
+        public decimal Salary { get; set; }
+        public override decimal GetAmountToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(@"{0}
+                Salary........: {1,18:C2}",
+                base.ToString(), GetAmountToPay());
+        }
+    }
+}
